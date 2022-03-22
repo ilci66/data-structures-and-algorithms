5f5090281dbb58d815385155984474a078ff707f
unknown <akbiyik.ilker7@gmail.com>
2022-03-22 17:48:56 +0100

    added iterative reverse method

--- linkedList/SinglyLinkedList.py
+++ linkedList/SinglyLinkedList.py
@@ -24,13 +24,14 @@ class LinkedList:
             temp = temp.next
         return temp.data
 
-    def getSize(self):
+    def get_size(self):
         size = 0
         temp = self.head
         while(temp):
             size += 1
             temp = temp.next
         print ("size is: ", size)
+        return size
 
     # going one by one printing the values
     def printList(self):
@@ -85,35 +86,87 @@ class LinkedList:
             temp = temp.next
             nth += 1
 
+    # got a little fancy with this one
     def erase(self, index):
-        temp = self.head
-        nth = 0
-        while(temp):
-            if(nth == index):
-                print(temp.data,index, nth)
-                #handle this after your break
+        if(index < 0):
+            print('index can\'t be negative')
+        if(index == 0):
+            self.pop_front()
 
-            temp = temp.next
+        nth = 1
+        prev = self.head
+        current = self.head.next
+
+        while(current):
+            if(index == nth):
+                prev.next = current.next
+                current.next = None
+                return
+            nth += 1
+            prev = current
+            current = current.next
+        print("out of bounds")
+
+    # value_n_from_end(n) - returns the value of the node at nth position from the end of the list
+    # I decided to start the indexing from 0, so zeroth is the last one
+    def value_n_from_end(self, index):
+        nth = 1
+        size = self.get_size()
+        wanted = size - index
+        current = self.head
+        if (wanted == 0):
+            print("wanted data is: ", current.data)
+            return current.data
+        while(current):
+            if(nth == wanted):
+                print("returning: ", current.data)
+                return current.data
+
+            current = current.next
             nth += 1
 
 
+    # Going with the iterative way of doing this because the space complexity is O(1)
+    # def reverse(self): # Iterative
+    #     prev = None
+    #     current = self.head
+    #     while(current):
+    #         next = current.next
+    #         current.next = prev
+    #         prev = current
+    #         current = next
+    #         # print("current", current.data)
+    #     self.head = prev # I forgot about the head for like 10 mins
+    #
+    def reverse(self, node): # recursive
+        if(node == None):
+            return node
+        if(node.next):
+            return node
 
+        node1 = self.reverse(node.next) # call recursively until we hit the base
+        node.next.next = node # points to itself
+        node.next = None
+        return node1
 
 if __name__ == '__main__':
     # initialize with an empty list
     s_l_list = LinkedList()
 
-    # the first node
+    # the first node, gotta define the head
     s_l_list.head = Node(1)
+
     second = Node(2)
     third = Node(3)
     fourth = Node(4)
+
+    # chain'em up
     s_l_list.head.next = second
     second.next = third
     third.next = fourth
 
     # s_l_list.printList()
-    # s_l_list.getSize()
+    # s_l_list.get_size()
     # s_l_list.isEmpty()
     # s_l_list.valueAt(0)
     # s_l_list.valueAt(2)
@@ -134,5 +187,10 @@ if __name__ == '__main__':
     #s_l_list.insert(1, 77)
     #s_l_list.printList()
 
-    s_l_list.erase(1)
+    # s_l_list.erase(2)
+    # s_l_list.printList()
+
+    # s_l_list.value_n_from_end(0)
+    # s_l_list.reverse() # for the iterative method
+    s_l_list.reverse(s_l_list.head) # call the method with the current head
     s_l_list.printList()
