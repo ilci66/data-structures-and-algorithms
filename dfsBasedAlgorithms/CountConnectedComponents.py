b08f89e1ec0eb77de2fcdf134930a312519fbd0e
unknown <akbiyik.ilker7@gmail.com>
2022-07-15 15:56:36 +0200

    rewrote myself

--- dfsBasedAlgorithms/CountConnectedComponents.py
+++ dfsBasedAlgorithms/CountConnectedComponents.py
@@ -4,28 +4,26 @@ class Graph:
         self.V = V
         self.adj = [[] for i in range(V)]
 
-    def DFSUtil(self, temp, v, visited):
+    def addEdge(self, v, w):
+        self.adj[v].append(w)
+        self.adj[w].append(v)
 
-        visited[v] = True
+    def DFSUtil(self, temp, v, visited):
         temp.append(v)
+        visited[v] = True
         for i in self.adj[v]:
             if visited[i] == False:
                 temp = self.DFSUtil(temp, i, visited)
         return temp
 
-    def addEdge(self, v, w):
-        self.adj[v].append(w)
-        self.adj[w].append(v)
 
     def connectedComponents(self):
-        visited = []
+        visited = [False] * self.V
         cc = []
         for i in range(self.V):
-            visited.append(False)
-        for v in range(self.V):
-            if visited[v] == False:
+            if visited[i] == False:
                 temp = []
-                cc.append(self.DFSUtil(temp, v, visited))
+                cc.append(self.DFSUtil(temp, i, visited))
         return cc
 
 
